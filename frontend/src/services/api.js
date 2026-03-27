58f48c550c0a1d5ccb6b84292f5300ceadb67b97
vernittyagi <vernittyagi@Vernits-MacBook-Air.local>
2026-03-27 15:00:21 +0530

    correcting backend baseURL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-    baseURL: 'http://localhost:3000/api'
+    baseURL: 'https://backend-chat-app-di8u.onrender.com/api'
 })
 
 export const generateTrip = (data) => {
